4892705c0a823dc48835cba070977368e8795f44
Darren Jensen <darren.jensen@gmail.com>
2023-11-09 07:12:57 +0700

    added friendtech and deposit contracts

---
+++ src/Deposit.sol
@@ -0,0 +1,15 @@
+contract Deposit {
+    address public seller = msg.sender;
+    mapping(address => uint256) public balance;
+
+    function deposit() external payable {
+        balance[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+        uint256 amount = balance[msg.sender];
+        balance[msg.sender] = 0;
+        (bool s, ) = msg.sender.call{value: amount}("");
+        require(s, "failed to send");
+    }
+}
---
+++ test/FriendTechSharesV1.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {FriendtechSharesV1} from "../src/FriendtechSharesV1.sol";
+
+contract FriendtechSharesV1Test is Test {
+    FriendtechSharesV1 public friendtech;
+
+    function setUp() public {
+        counterfriendtech = new FriendtechSharesV1();
+    }
+}
